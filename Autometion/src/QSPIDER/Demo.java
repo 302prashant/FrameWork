111a2b4f54968f3cfaf405dee26e563a5e1ae1ce
302prashant <115981249+302prashant@users.noreply.github.com>
2022-10-18 19:28:05 +0530

    Update Demo.java

--- Autometion/src/QSPIDER/Demo.java
+++ Autometion/src/QSPIDER/Demo.java
@@ -10,6 +10,7 @@ public class Demo {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
+		System.out.println("opning the browser");
 		driver.close();
 	}
 
